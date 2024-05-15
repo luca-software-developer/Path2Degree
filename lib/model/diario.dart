67502d74187dc078a9bdb8b955364f8f836fd0d0
Luca Dello Russo <lucadellorusso5@gmail.com>
2024-05-15 23:20:10 +0200

    Database

--- lib/model/diario.dart
+++ lib/model/diario.dart
@@ -1,18 +1,19 @@
 class Diario {
   final String nome;
+  final String testo;
 
-  const Diario({required this.nome});
+  const Diario({required this.nome, required this.testo});
 
   Map<String, Object?> toMap() {
-    return {'nome': nome};
+    return {'nome': nome, 'testo': testo};
   }
 
-  Diario fromMap(Map<String, Object?> map) {
-    return Diario(nome: map['nome'] as String);
+  factory Diario.fromMap(Map<String, Object?> map) {
+    return Diario(nome: map['nome'] as String, testo: map['testo'] as String);
   }
 
   @override
   String toString() {
-    return 'Luogo { nome: $nome }';
+    return 'Luogo { nome: $nome, testo: $testo }';
   }
 }
--- lib/model/esame.dart
+++ lib/model/esame.dart
@@ -1,40 +1,68 @@
 import 'package:path2degree/model/tipologia.dart';
+import 'package:path2degree/model/diario.dart';
+import 'package:path2degree/model/categoria.dart';
 
 class Esame {
   final String nome;
+  final String corsoDiStudi;
   final int cfu;
   final DateTime dataOra;
+  final String luogo;
   final Tipologia tipologia;
+  final String docente;
   final int voto;
+  final bool lode;
+  final List<Categoria> categorie;
+  final Diario diario;
 
-  const Esame(
-      {required this.nome,
-      required this.cfu,
-      required this.dataOra,
-      required this.tipologia,
-      required this.voto});
+  const Esame({
+    required this.nome,
+    required this.corsoDiStudi,
+    required this.cfu,
+    required this.dataOra,
+    required this.luogo,
+    required this.tipologia,
+    required this.docente,
+    required this.voto,
+    required this.lode,
+    required this.categorie,
+    required this.diario,
+  });
 
-  Map<String, Object?> toMap() {
-    return {
-      'nome': nome,
-      'cfu': cfu,
-      'dataOra': dataOra,
-      'tipologia': tipologia,
-      'voto': voto
-    };
+  factory Esame.fromMap(Map<String, dynamic> map) {
+    return Esame(
+      nome: map['nome'] as String,
+      corsoDiStudi: map['corsoDiStudi'] as String,
+      cfu: map['cfu'] as int,
+      dataOra: DateTime.parse(map['dataOra'] as String),
+      luogo: map['luogo'] as String,
+      tipologia: map['tipologia'] as Tipologia,
+      docente: map['docente'] as String,
+      voto: map['voto'] as int,
+      lode: map['lode'] as bool,
+      categorie: map['categorie'] as List<Categoria>,
+      diario: map['diario'] as Diario,
+    );
   }
 
-  Esame fromMap(Map<String, Object?> map) {
-    return Esame(
-        nome: map['nome'] as String,
-        cfu: map['cfu'] as int,
-        dataOra: map['dataOra'] as DateTime,
-        tipologia: map['tipologia'] as Tipologia,
-        voto: map['voto'] as int);
+  Map<String, dynamic> toMap() {
+    return {
+      'nome': nome,
+      'corsoDiStudi': corsoDiStudi,
+      'cfu': cfu,
+      'dataOra': dataOra,
+      'luogo': luogo,
+      'tipologia': tipologia,
+      'docente': docente,
+      'voto': voto,
+      'lode': lode,
+      'categorie': categorie,
+      'diario': diario,
+    };
   }
 
   @override
   String toString() {
-    return 'Esame { nome: $nome, cfu: $cfu, dataOra: $dataOra, tipologia: $tipologia, voto: $voto }';
+    return 'Esame { nome: $nome, corsoDiStudi: $corsoDiStudi, cfu: $cfu, dataOra: $dataOra, luogo: $luogo, tipologia: $tipologia, docente: $docente, voto: $voto, lode: $lode, diario: $diario, categorie: $categorie }';
   }
 }
